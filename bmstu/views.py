566e1c59f2633d055efd9d51eb73c02ca07f6adc
addtrees <shencr0929@gmail.com>
2023-03-24 01:02:40 +0300

    homework v1.0, it work,but not bery well

---
+++ bmstu/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+import onnxruntime
+import numpy as np
+from PIL import Image
+
+imageClassList = {'0': ['car'], '1': ['plane'], '2': ['ship']}  #Сюда указать классы
+
+def scoreImagePage(request):
+    return render(request, 'scorepage.html')
+
+def predictImage(request):
+    fileObj = request.FILES['filePath']
+    fs = FileSystemStorage()
+    filePathName = fs.save('images/'+fileObj.name,fileObj)
+    filePathName = fs.url(filePathName)
+    modelName = request.POST.get('modelName')
+    scorePrediction = predictImageData(modelName, '.'+filePathName)
+    context = {'scorePrediction': scorePrediction}
+    return render(request, 'scorepage.html', context)
+
+def predictImageData(modelName, filePath):
+    img = Image.open(filePath).convert("RGB")
+    img = np.asarray(img.resize((32, 32), Image.ANTIALIAS))
+    sess = onnxruntime.InferenceSession(r'/home/shen/PycharmProjects/Homework1_Anton/media/models/linear_1.onnx') #<-Здесь требуется указать свой путь к модели
+    outputOFModel = np.argmax(sess.run(None, {'input': np.asarray([img]).astype(np.float32)}))
+    score = imageClassList[str(outputOFModel)]
+    return score
